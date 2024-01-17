4a796df8c833003c920ea7ae80e21adcef80e8f6
kanisorn <qawqaw63123@gmail.com>
2024-01-17 19:58:09 +0700

    welcome function successful

--- StudentBackendAPI.py
+++ StudentBackendAPI.py
@@ -1,16 +1,13 @@
 
 from DB import ShowAllStudentRecord
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    all_students = ShowAllStudentRecord()
-    for std in all_students:
-        print(std)
-    return "Hello World!"
+def Welcome():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
