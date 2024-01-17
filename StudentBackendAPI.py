d4f1eea6439cfe21ad2d897fc88cba2280b0c54b
kanisorn <qawqaw63123@gmail.com>
2024-01-17 20:05:01 +0700

    edit show studens

--- StudentBackendAPI.py
+++ StudentBackendAPI.py
@@ -9,5 +9,12 @@ app = Flask(__name__)
 def Welcome():
     return render_template("index.html")
 
+@app.route("/students")
+def ShowAllStudents():
+    students = ShowAllStudentRecord()
+    for student in students:
+        return jsonify(student)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
